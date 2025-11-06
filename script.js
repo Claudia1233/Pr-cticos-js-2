c2257becac5de3fd2a58070fe6ed88091ca32e56
Claudia1233 <c87093899@gmail.com>
2025-11-05 23:31:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ function ejecutarEjercicio() {
 
   // Mostrar mensaje según el valor
   if (valor === 100) {
-    resultado.textContent = `🎉 ¡Felicitaciones! El valor generado fue ${valor}, exactamente igual a 100.`;
+    resultado.textContent = ` ¡Felicitaciones! El valor generado fue ${valor}, exactamente igual a 100.`;
     resultado.style.color = "#155724";
     resultado.style.backgroundColor = "#e6ffed";
     resultado.style.borderColor = "#b6e8c1";
   } else {
-    resultado.textContent = `ℹ️ El valor generado fue ${valor}, y no es igual a 100.`;
+    resultado.textContent = ` El valor generado fue ${valor}, y no es igual a 100.`;
     resultado.style.color = "#856404";
     resultado.style.backgroundColor = "#fff3cd";
     resultado.style.borderColor = "#ffeeba";
